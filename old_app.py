ec73c0ae9f119cb3e3b64a6b0684a9c2a7c9200b
n23006266 <malo.neuve-eglise@etu.univ-amu.fr>
2024-12-10 10:15:03 +0100

    Retour en méthode post, old_app.py, route /old, HTML et JS adapté à la méthode post rétablis

---
+++ old_app.py
@@ -0,0 +1,23 @@
+from flask import Flask, Response, request, jsonify, render_template
+
+app = Flask(__name__)
+user_data = {}
+
+
+@app.route("/old")
+def old():
+    return render_template("old_index.html")
+
+
+@app.route("/data", methods=["POST"])
+def receive_data():
+
+    data = request.json
+    user_data["last_received"] = data
+    print(f"Données reçues : {data}")
+
+    return jsonify({"status": "success", "received": data})
+
+
+if __name__ == "__main__":
+    app.run(host="::1", port=1120)
---
+++ static/js/old_index.js
@@ -0,0 +1,77 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const permissionButton = document.getElementById('permissionButton');
+    const dataContainer = document.querySelector('.data-container');
+    const startStopButton = document.getElementById('start_stop');
+    const sendStatus = document.getElementById('sendStatus');
+
+    const url = 'https://fenouil.aioli.ec-m.fr';
+    const delay = 2000;
+
+    let accX = 0;
+    let accY = 0;
+    let accZ = 0;
+
+    let envoi = false;
+
+    function recup_acc(event) {
+        accX = event.accelerationIncludingGravity.x || 0;
+        accY = event.accelerationIncludingGravity.y || 0;
+        accZ = event.accelerationIncludingGravity.z || 0;
+
+        document.getElementById('accX').innerText = accX.toFixed(1);
+        document.getElementById('accY').innerText = accY.toFixed(1);
+        document.getElementById('accZ').innerText = accZ.toFixed(1);
+    }
+
+    function sendDataToServer() {
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ "x": accX, "y": accY, "z": accZ })
+        })
+            .then(response => response.json())
+            .then(data => console.log("Données envoyées avec succès:", data))
+            .catch(error => console.error("Erreur d'envoi des données:", error));
+    }
+
+    function main() {
+        permissionButton.style.display = 'none';
+        dataContainer.style.display = 'block';
+        window.addEventListener('devicemotion', recup_acc);
+        startStopButton.addEventListener('click', function () {
+            if (!envoi) {
+                intervalId = setInterval(sendDataToServer, delay);
+                envoi = true;
+                sendStatus.textContent = "actif";
+            } else {
+                clearInterval(intervalId);
+                envoi = false;
+                sendStatus.textContent = "inactif";
+            }
+        });
+    }
+
+    // Vérification de l'API DeviceMotionEvent et demande de permission pour iOS > 13
+    if (typeof DeviceMotionEvent !== 'undefined') {
+        if (typeof DeviceMotionEvent.requestPermission === 'function') {
+            permissionButton.addEventListener('click', function () {
+                DeviceMotionEvent.requestPermission().then(function (permissionState) {
+                    if (permissionState === 'granted') {
+                        main();
+                    } else {
+                        alert('L\'autorisation d\'accès aux capteurs a été refusée.');
+                    }
+                }).catch(function (error) {
+                    console.error('Erreur lors de la demande d\'autorisation :', error);
+                    alert('Une erreur est survenue lors de la demande d\'autorisation.');
+                });
+            });
+        } else {
+            main();
+        }
+    } else {
+        alert('API DeviceMotionEvent non supportée');
+    }
+});
