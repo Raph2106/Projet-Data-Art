77dba79afaacaea3affe7afa42e01f3e73117655
n23006266 <malo.neuve-eglise@etu.univ-amu.fr>
2024-12-11 17:59:59 +0100

    heatmap constrastée

--- chaleur.py
+++ chaleur.py
@@ -169,7 +169,7 @@ class HeatSimulation3DfromClaude:
         # 'hot' va du noir au rouge puis au jaune puis au blanc
         cax = ax.imshow(
             self.grid,
-            cmap="seismic",
+            cmap="coolwarm",
             origin="lower",
             interpolation="nearest",
             vmin=0,
