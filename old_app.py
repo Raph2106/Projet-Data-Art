2d56dec2ea54f2ac968d850c5d5e096d5f4ba7e7
n23006266 <malo.neuve-eglise@etu.univ-amu.fr>
2024-12-11 18:29:33 +0100

    inversion de app et old_app

--- old_app.py
+++ old_app.py
@@ -1,15 +1,9 @@
-from flask import Flask, Response, request, jsonify, render_template
-from chaleur import HeatSimulation3D, HeatSimulation2D
-import logging
-import matplotlib.pyplot as plt
-import io
+from flask import Flask, Response, render_template, request
+from flask_socketio import SocketIO, emit
+from chaleur import generate_frame
 import time
-import redis
-import json
-
-app = Flask(__name__)
-app.debug = True
-r = redis.StrictRedis(host="localhost", port=6379, db=0)
+import logging
+import threading
 
 logging.getLogger("matplotlib").setLevel(logging.WARNING)
 logging.basicConfig(
@@ -18,78 +12,72 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
 
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret_key"
+app.debug = True
 
-def get_data():
-    keys = r.keys("user_data_*")
-    all_data = []
+socketio = SocketIO(app)
 
-    for key in keys:
-        while True:
-            data = r.lpop(key)
-            if not data:
-                break
-            all_data.append(json.loads(data))
-
-    return all_data
+shared_data = {"x": 1.0}
+test_data = {"x": 2.0}
+data_lock = threading.Lock()
 
 
-def generate_frame():
-
-    plt.ioff()
-    data = {}
-    sim = HeatSimulation2D(150, 150, diffusion_rate=0.15)
-    fig, ax = plt.subplots(figsize=(10, 8))
-
-    while True:
-        t0 = time.time()
-        data = get_data()
-        for d in data:
-            sim.add_heat_source2(
-                round(float(d["x"])),
-                round(float(d["y"])),
-                temperature=round(abs(float(d["z"]))),
-                radius=round(float(d["z"])),
-            )
-        sim.update()
-        sim.visualize_2d(ax)
-
-        buf = io.BytesIO()
-        plt.savefig(buf, format="jpeg", bbox_inches="tight")
-        buf.seek(0)
-
-        frame = buf.read()
-        yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
-
-        buf.close()
-        t1 = time.time()
-        if t1 - t0 < 0.04:
-            time.sleep(0.04 - (t1 - t0))
+def get_shared_data():
+    with data_lock:
+        return shared_data
 
 
-@app.route("/old")
-def old():
-    return render_template("old_index.html")
+@app.context_processor
+def inject_time():
+    return {"time": time.time}
 
 
-@app.route("/data", methods=["POST"])
-def receive_data():
-    data = request.json
-    # user_data.put(data)
-    unique_key = f"user_data_{int(time.time() * 1000)}"
-    json_data = json.dumps(data)
-    r.rpush(unique_key, json_data)
-    print(f"Données reçues : {data}")
-
-    return jsonify({"status": "success", "received": data})
+@app.route("/websocket")
+def index():
+    return render_template("index.html")
 
 
-@app.route("/video_stream")
+@app.route("/post", methods=["POST"])
+def update_data():
+    test_data["x"] = request.json["x"]
+    return {"Données reçues": test_data}
+
+
+# mettre route /websocket
+@app.route("/video_feed")
+def video_feed():
+    return render_template("video_feed.html")
+
+
+@app.route("/test")
+def testFlask():
+    return Response(response="Réponse de Flask. \n")
+
+
+@app.route("/websocket_video_stream")
 def video_stream():
     return Response(
-        generate_frame(),
+        generate_frame(get_shared_data),
         mimetype="multipart/x-mixed-replace; boundary=frame",
     )
 
 
+@socketio.on("send_data")
+def handle_send_data(data):
+    global shared_data
+    with data_lock:
+        shared_data["x"] = data["x"]
+    print("Données reçues:", data)
+    emit("data_response", {"status": "Données reçues avec succès"})
+
+
+@socketio.on("start_video")
+def start_video():
+    for frame_base64 in generate_frame():
+        emit("new_frame", {"image": frame_base64})
+        time.sleep(0.04)
+
+
 if __name__ == "__main__":
-    app.run(host="::1", port=1120)
+    socketio.run(app, host="::1", port=1120)
